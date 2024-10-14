93b930a33c14cddd5995ee18985b3463a2fad36f
dylantizguine <dylantizguine@gmail.com>
2024-10-14 16:07:16 +0200

    Initial commit

---
+++ src/main/java/com/project/rexPref/RexPrefApplication.java
@@ -0,0 +1,13 @@
+package com.project.rexPref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexPrefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RexPrefApplication.class, args);
+	}
+
+}
